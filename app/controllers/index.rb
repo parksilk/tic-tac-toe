d53553487d2b40ffdc2cee91f20df152779306f3
Apprentice <apprentice@dbc01.local>
2013-03-29 14:21:15 -0700

    session force redirect

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,13 @@
+before do
+
+  if session[:user].nil?
+    unless request.path_info = "/login" || request.path_info = "/signup"
+      redirect '/'
+    end
+  end
+
+end
+
 get '/' do
   # Look in app/views/index.erb
 
