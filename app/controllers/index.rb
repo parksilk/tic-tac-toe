8fd6000113e54fa12ef5e38b4119189d04dcaed8
Apprentice <apprentice@dbc01.local>
2013-03-29 15:22:22 -0700

    signup page forms

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,5 @@
 before do
-
-  if session[:user].nil?
-    unless request.path_info = "/login" || request.path_info = "/signup"
-      redirect '/'
-    end
-  end
-
+  
 end
 
 get '/' do
@@ -15,20 +9,30 @@ get '/' do
 end
 
 get '/game' do
+  redirect '/' if !current_user
   @game = Game.new
 
   erb :game
 end
 
 post '/login' do
+
   login
 end
 
 post '/signup' do
+ 
   signup
 end
 
 get '/games' do
+  redirect '/' if !current_user
   p current_user.inspect
   current_user.inspect
 end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
